fed0c9f6fb6b05dee92ac31941c70657fc15e20c
07huan <1791057498mila@gmail.com>
2026-07-16 00:42:44 +0800

    learn input

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,5 +1,5 @@
-name = input("请输入你的名字：")
-age = input("请输入你的年龄：")
+name=input("请输入你的名字：")
+age=input("请输入你的年龄")
 
-print("你好", name)
-print("你的年龄是", age)
+print("你的名字是",name,"同学")
+print("你的年龄是：",age,"岁")
