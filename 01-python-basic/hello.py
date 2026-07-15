77418e1d5f20b1b184134b25f5ba0846b1e13eab
07huan <1791057498mila@gmail.com>
2026-07-16 00:57:36 +0800

    make calculator

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,4 +1,7 @@
-num1=float(input("请输入第一个数字："))
-num2=float(input("请输入第二个数字："))
+num1 = float(input("请输入第一个数字："))
+num2 = float(input("请输入第二个数字："))
 
-print("num1+num2=",num1+num2)
+print("加法结果：", num1 + num2)
+print("减法结果：", num1 - num2)
+print("乘法结果：", num1 * num2)
+print("除法结果：", num1 / num2)
