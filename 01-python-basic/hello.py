3d14bc128a9f12160c7680a346b82fc67dc811b6
07huan <1791057498mila@gmail.com>
2026-07-19 04:57:45 +0800

    完成数字小游戏

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,6 +1,12 @@
-num = 1
+import random
+num = random.randint(0,100)
 
-while num <= 10:
-    if num % 2 == 0:
-        print(num)
-    num += 1
+while True:
+    guess = int(input("请输入你的数字："))
+    if guess > num:
+        print("你猜大了")
+    elif guess < num:
+        print("你猜小了")
+    else:
+        print("你猜对了")
+        break
