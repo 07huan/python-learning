f593bc0214804dfeabc24674ca1d52c528dd20fd
07huan <1791057498mila@gmail.com>
2026-07-19 04:15:46 +0800

    学习内容

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,9 +1,6 @@
-scores = [60, 75, 88, 92, 55]
+num = 1
 
-for score in scores:
-    if   60 <= score <= 70:
-        print(score,"及格")
-    elif 70 < score < 100 :
-        print(score,"优秀")
-    else:
-        print(score,"不合格")
+while num <= 10:
+    if num % 2 == 0:
+        print(num)
+    num += 1
