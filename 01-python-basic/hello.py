b4fb95744e074fa089be3072d96b1e90670054b3
07huan <1791057498mila@gmail.com>
2026-07-21 02:27:50 +0800

    learn dictionary

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,12 +1,8 @@
-scores = [70, 55, 90, 40, 85]
-for score in scores:
-    print(score)
-count=0
-for score in scores:
-    if score >= 60:
-        print(score,"合格")
-        count += 1
-    else:
-        print(score,"不合格")
+student={
+    "name" : "小明",
+    "scores" : [66,82,93,49]
+}
 
-print(f"合格人数为：{count}")
+print(student["name"])
+for score in student["scores"]:
+    print(score)
