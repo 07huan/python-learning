00a7705017bc776ffb314a17c36f48b86e72e335
07huan <1791057498mila@gmail.com>
2026-07-22 02:56:09 +0800

    learn function

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,8 +1,11 @@
-student={
-    "name" : "小明",
-    "scores" : [66,82,93,49]
-}
-
-print(student["name"])
-for score in student["scores"]:
-    print(score)
+def scores(cj):
+    count = 0
+    for score in cj:
+        if score >= 60:
+            count += 1
+        
+    return count
+          
+cj = [70,55,90,40,85]
+result = scores(cj)
+print(result)
