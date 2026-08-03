67b500d6a6dc29f05bb30819107f00a92bf7577d
07huan <1791057498mila@gmail.com>
2026-08-03 22:54:32 +0800

    complete function encapsulation practice

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,28 +1,17 @@
 students = [
-    {"name": "Amy", "score": 90},
-    {"name": "Tom", "score": 40},
-    {"name": "Tim", "score": 69},
-    {"name": "Jerry", "score": 87}
+    {"name": "amy", "score": 90},
+    {"name": "john", "score": 70},
+    {"name": "tom", "score": 49},
+    {"name": "jim", "score": 90}
 ]
 
-total = 0
-count = 0
+def calculate_total(students):
+    total = 0
 
-with open("成绩报告.txt", "w") as file:
     for student in students:
         total += student["score"]
 
-        if student["score"] >= 60:
-            count += 1
-            result = "合格"
-        else:
-            result = "不合格"
+    return total / len(students)
 
-        file.write(
-            f"{student['name']} 成绩：{student['score']} {result}\n"
-        )
 
-    average = total / len(students)
-
-    file.write(f"合格人数：{count}人\n")
-    file.write(f"平均分：{average}")
+print(calculate_total(students))
