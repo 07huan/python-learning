6dcec05b39d3e35c92fe71d17f3997f2d867f723
07huan <1791057498mila@gmail.com>
2026-08-04 22:36:35 +0800

    complete student management system

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,17 +1,39 @@
-students = [
-    {"name": "amy", "score": 90},
-    {"name": "john", "score": 70},
-    {"name": "tom", "score": 49},
-    {"name": "jim", "score": 90}
+students =[
+    {"name":"john","score":60},
+    {"name":"amy","score":94},
+    {"name":"mike","score":78},
+    {"name":"jane","score":85}
 ]
 
-def calculate_total(students):
+def show_student(students):
+    for student in students:
+        print(f"学生名单{student['name']}")
+
+def hege(students):
+    for student in students:
+        if student["score"] >=60:
+            print(f"{student['name']}同学及格了")    
+
+def calculate_average(students):
     total = 0
+    for student in students:
+        total += student["score"]             
+    average = total / len(students)
+    print(f"平均成绩是{average}")
+
+def count_fail(students):
+    no = 0
 
     for student in students:
-        total += student["score"]
+        if student["score"] < 60:
+            no += 1
+            print(f"{student['name']}同学不及格。")
 
-    return total / len(students)
+    print(f"所以不合格的人数为：{no}")
 
-
-print(calculate_total(students))
+def main():
+    show_student(students)
+    hege(students)
+    calculate_average(students)
+    count_fail(students)
+main()
