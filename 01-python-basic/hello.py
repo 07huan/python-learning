94dc00ae3632eb373dfe5f8c6b72eda966075c25
07huan <1791057498mila@gmail.com>
2026-08-07 23:17:38 +0800

    learn api basics

--- 01-python-basic/hello.py
+++ 01-python-basic/hello.py
@@ -1,39 +1,14 @@
-students =[
-    {"name":"john","score":60},
-    {"name":"amy","score":94},
-    {"name":"mike","score":78},
-    {"name":"jane","score":85}
-]
+question = input("请输入问题：")
+name = input("请输入名字：")
+user = {
+    "name": name,
+    "question": question
+    
+}
 
-def show_student(students):
-    for student in students:
-        print(f"学生名单{student['name']}")
+response = {
+    "answer": "Python是一种简单易学的编程语言"
+}
 
-def hege(students):
-    for student in students:
-        if student["score"] >=60:
-            print(f"{student['name']}同学及格了")    
-
-def calculate_average(students):
-    total = 0
-    for student in students:
-        total += student["score"]             
-    average = total / len(students)
-    print(f"平均成绩是{average}")
-
-def count_fail(students):
-    no = 0
-
-    for student in students:
-        if student["score"] < 60:
-            no += 1
-            print(f"{student['name']}同学不及格。")
-
-    print(f"所以不合格的人数为：{no}")
-
-def main():
-    show_student(students)
-    hege(students)
-    calculate_average(students)
-    count_fail(students)
-main()
+print(f"用户{user['name']}的问题是{user['question']}")
+print(f"ai回答：{response['answer']}")
